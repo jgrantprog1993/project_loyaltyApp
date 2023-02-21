f4662ea6b13485911405cbec0386c865054a1574
jgrantprog1993 <jason.grant@umail.ucc.ie>
2023-02-21 22:28:03 +0000

    ++ Voucher Cards

--- src/components/voucherItem.js
+++ src/components/voucherItem.js
@@ -1,53 +1,43 @@
 // @ts-nocheck
 import Link from "next/link"
+import { getCookies, getCookie, setCookies, removeCookies } from 'cookies-next';
 
-export default function VoucherItem({voucher,vouchersFromMe}) {
-    const voucherPercntage = (voucher.voucherPoints / 10) * 100
-    console.log('voucher')
-    console.log(voucher)
+export default function VoucherItem({voucher}) {
     
-    console.log('vouchersFromMe')
-    console.log(vouchersFromMe)
-    var VoucherResult = vouchersFromMe.filter(a => a.id === voucher.id)
+    // console.log('voucher')
+    // console.log(voucher)
+    // console.log('voucher.attributes.voucher.voucherPoints')
+    // console.log(voucher.attributes.voucher.data)
+   
+    var voucherPoints = voucher.attributes.voucher.data.attributes.voucherPoints
+    // console.log('voucherPoints')
+    // console.log(voucherPoints)
+    const voucherPointsPec = (voucherPoints/10) * 100
+   
     
-    console.log('VoucherResult')
-    console.log(VoucherResult)
-    
+
     return(
+        <>
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <img class="rounded-t-lg" src="public/images/place-arch-coffee-exterior-2-768x1024.jpeg" alt="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">vouchersFromMe</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{voucher.attributes.name}</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Category</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{voucher.attributes.category}</p>
+                </div>  
+                
+                <div class="p-5"> 
                 <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style={{width: voucherPercntage+'%'}}></div>
+                    <div class="bg-blue-600 h-2.5 rounded-full" style={{width: voucherPointsPec+'%'}}></div>
                 </div>
             
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{voucher.voucherPoints}/ 10</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{voucherPoints}/ 10</p>
             </div>
-        </div>
+            </div>
+        </>
     )
 }
 
-export async function getServerSideProps({req, res})  {
-	const cookieToken = getCookie('token', { req, res})
-	
-    console.log(voucher)
-	const response = await fetch(`${API_URL}api/locations?populate=*`,
-	{
-		method: 'GET',
-			headers: {
-				Authorization:`Bearer ${cookieToken}`
-			}
-	})
-	const vouchersFromMe = await response.json()
-    
-   
-	return {
-		props: {vouchersFromMe}
-	}
-}
--- src/pages/vouchers/index.js
+++ src/pages/vouchers/index.js
@@ -2,33 +2,74 @@
 import Layout from "../../components/layout"
 import { API_URL } from "../../utils/config"
 import VoucherItem from "../../components/voucherItem"
-import { getCookies, getCookie, setCookies, removeCookies } from 'cookies-next';
 
-export default function Vouchers({vouchers}) {
-	//console.log(vouchers)
+import { getCookies, getCookie, setCookies, removeCookies } from 'cookies-next';
+import { useContext } from 'react'
+import AuthContext from '../../context/AuthContext'
+//const {user} = useContext(AuthContext)
+
+export default function Vouchers({vouchers,locations}) {
+	const {user} = useContext(AuthContext)
+
+//  console.log('user')
+//  console.log(user)
+//  console.log(user?.id)
+//  console.log('locations')
+//  console.log(locations)
+	// const vouchersData0 = vouchers.vouchers[0]
 	const vouchersData = vouchers.vouchers
-	//console.log(vouchersData)
+	// console.log('vouchersData')
+	// console.log(vouchersData)
+	 const locationsData = locations.data
+	//  console.log('locationsData')
+	// console.log(locationsData)
+	// console.log('locationsDataattributes.users_permissions_user.id')
+	// console.log(locationsData[0].attributes.users_permissions_user.data.id)
+	// //object = locationsData.findAll(obj => obj.id === vouchersData.id);
+	const items = locationsData.filter(item => {
+		return item?.attributes?.users_permissions_user?.data?.id === user?.id
+	})
+	// console.log('items')
+ 	// console.log(items)
+	// const objects = []
+	
+	// for(var i=0; i<vouchersData.length; i++)
+	// {
+	// 	objects[i] = objects[i]
+	// }
+	// console.log('objects')
+ 	// console.log(objects)
   return (
 	  <Layout title='Vouchers' keywords='{undefined}' description='{undefined}' >
 					<div className='my-20 h-screen'>
 						<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-							Vouchers
+							Vochers
 						</h2>
 
 						<p className='text-zinc-600 dark:text-zinc-400'></p>
 						{vouchersData.length===0 && <h3> No Vouchers to show </h3>}
+						<>
 						
-						{vouchersData.map((voucher) => (
-							<VoucherItem key={vouchersData.id} voucher={voucher}/>
-						))}
+						
+						
+							{items.map((item) => (
+								<>
+									<VoucherItem voucher={item} />
+								</>	
+								))
+							}
+										
+						
+						</>
+						
+						
 					</div>
     </Layout>
   )
 }
 
-export async function getServerSideProps({req, res})  {
+export async function getServerSideProps({req, res}) {
 	const cookieToken = getCookie('token', { req, res})
-	//console.log(cookieToken)
 	const response = await fetch(`${API_URL}/api/users/me?populate=*`,
 	{
 		method: 'GET',
@@ -37,9 +78,17 @@ export async function getServerSideProps({req, res})  {
 			}
 	})
 	const vouchers = await response.json()
-   
-   
+	console.log(vouchers)
+	const response2 = await fetch(`${API_URL}/api/locations?populate=*`,
+	{
+		method: 'GET',
+			headers: {
+				Authorization:`Bearer ${cookieToken}`
+			}
+	})
+	const locations = await response2.json()
+	
 	return {
-		props: {vouchers}
+		props: {vouchers:vouchers,locations:locations}
 	}
 }
